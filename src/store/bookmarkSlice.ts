66c9a45d48d98fe5b1d3905b8e2dab5ddb4a1bd5
StoicGoose <josh@jwolmedia.com>
2025-11-22 19:50:42 -0700

    fixed initial filtering

--- src/store/bookmarkSlice.ts
+++ src/store/bookmarkSlice.ts
@@ -3,8 +3,11 @@ import data from "./data.json";
 
 const filtersApplied: Array<string> = [];
 const filterMap: Map<string, number> = new Map();
+const unarchivedArray = data.bookmarks.filter(
+  (bookmark) => !bookmark.isArchived
+);
 
-for (const bookmark of data.bookmarks) {
+for (const bookmark of unarchivedArray) {
   for (const tag of bookmark.tags) {
     if (filterMap.has(tag)) {
       filterMap.set(tag, filterMap.get(tag)! + 1);
@@ -14,16 +17,22 @@ for (const bookmark of data.bookmarks) {
   }
 }
 
+const pinnedBookmarks = unarchivedArray.filter((bookmark) => bookmark.pinned);
+const unpinnedBookmarks = unarchivedArray.filter(
+  (bookmark) => !bookmark.pinned
+);
+
 const filterArray = Array.from(filterMap, ([key, value]) => ({ key, value }));
 
 filterArray.sort((a, b) => a.key.localeCompare(b.key));
 
 const bookmarkInitialState = {
   allBookmarks: data.bookmarks,
-  filteredBookmarks: data.bookmarks,
+  filteredBookmarks: [...pinnedBookmarks, ...unpinnedBookmarks],
   nextId: data.bookmarks.length + 1,
   filtersApplied,
   filters: filterArray,
+  showArchived: false,
 };
 
 const bookmarkSlice = createSlice({
